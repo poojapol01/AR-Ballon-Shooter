692c42bfce5c29a78d402d6b9f426ee80be2412f
poojapol01 <polpooja05@gmail.com>
2023-07-11 23:51:55 +0530

    Added AR and Balloon Packages

---
+++ Assets/Scripts/SpawnScript.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnScript : MonoBehaviour
+{
+    public Transform[] spawnPoints;
+    public GameObject[] balloons;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+}
