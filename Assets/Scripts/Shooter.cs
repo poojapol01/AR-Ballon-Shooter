b37f14d3a337b34d8064d7a6155b79d709d6dace
poojapol01 <polpooja05@gmail.com>
2023-07-25 21:02:04 +0530

    Added Ballon Burst Smoke Effect

--- Assets/Scripts/Shooter.cs
+++ Assets/Scripts/Shooter.cs
@@ -5,19 +5,18 @@ using UnityEngine;
 public class Shooter : MonoBehaviour
 {
     public GameObject arCamera;
+    public GameObject smoke;
 
     public void Shoot()
     {
         RaycastHit hit;
 
-        Debug.Log("Hitman2");
         if (Physics.Raycast(arCamera.transform.position, arCamera.transform.forward, out hit))
         {
-            Debug.Log("Hitman1");
             if (hit.transform.name == "Air_Balloon_1(Clone)" || hit.transform.name == "Air_Balloon_2(Clone)" || hit.transform.name == "Air_Balloon_3(Clone)")
             {
                 Destroy(hit.transform.gameObject);
-                Debug.Log("Hitman");
+                Instantiate(smoke, hit.point, Quaternion.LookRotation(hit.normal));
             }
         }
     }
