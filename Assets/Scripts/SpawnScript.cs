f1180f22d25716711b27626cc9fd0a7d8fce61d3
poojapol01 <polpooja05@gmail.com>
2023-07-27 11:26:30 +0530

    Auto destroy Balloons after a time span

--- Assets/Scripts/SpawnScript.cs
+++ Assets/Scripts/SpawnScript.cs
@@ -6,6 +6,8 @@ public class SpawnScript : MonoBehaviour
 {
     public Transform[] spawnPoints;
     public GameObject[] balloons;
+    private float destroyObjectTime = 20f;
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -20,5 +22,34 @@ public class SpawnScript : MonoBehaviour
             Instantiate(balloons[i], spawnPoints[i].position, spawnPoints[i].rotation);
         }
         StartCoroutine(StartSpawning());
+
+        Invoke("DestroyObject", destroyObjectTime);
+    }
+
+    private void DestroyObject()
+    {
+        if (GameObject.Find("Air_Balloon_1(Clone)") != null)
+        {
+            //it exists
+            Destroy(GameObject.Find("Air_Balloon_1(Clone)"));
+            Debug.Log("Destroyed First Ballon");
+        }
+        if (GameObject.Find("Air_Balloon_2(Clone)") != null)
+        {
+            //it exists
+            Destroy(GameObject.Find("Air_Balloon_2(Clone)"));
+            Debug.Log("Destroyed Second Ballon");
+        }
+        if (GameObject.Find("Air_Balloon_3(Clone)") != null)
+        {
+            //it exists
+            Destroy(GameObject.Find("Air_Balloon_3(Clone)"));
+            Debug.Log("Destroyed Third Ballon");
+        }
+        if (GameObject.Find("msVFX_Stylized Smoke 5(Clone)") != null)
+        {
+            //it exists
+            Destroy(GameObject.Find("msVFX_Stylized Smoke 5(Clone)"));
+        }
     }
 }
